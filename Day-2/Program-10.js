92f63bec7d866342f546cc8904677ab58452d674
AnTriKsH <106952116+AntrikshSaini13@users.noreply.github.com>
2024-11-19 13:40:17 +0530

    update

--- Day-2/Program-10.js
+++ Day-2/Program-10.js
@@ -1,1 +1,17 @@
 // Switch Condition 
+let num = 1;
+
+switch (num){
+    case 1:{
+        console.log("Aman");
+        break;
+    }
+    case 2:{
+        console.log("Aman Saii");
+        break;
+    }
+    case 3:{
+        console.log("Aman Kumar");
+        break;
+    }
+}
