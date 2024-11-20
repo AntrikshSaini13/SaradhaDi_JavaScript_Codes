e208f4b8e1dcd430c97047782da1741d7f8d5953
AnTriKsH <106952116+AntrikshSaini13@users.noreply.github.com>
2024-11-20 23:34:20 +0530

    update

---
+++ Day-4/demo.js
@@ -0,0 +1,7 @@
+let arrayOfFruit = ["apple","mango","Graphs","apple"];
+
+for (let i = 0; i<arrayOfFruit.length;i++){
+    document.getElementById("demo").innerHTML = arrayOfFruit + " is found in position " + i;
+    // document.getElementById("demo").innerHTML = arrayOfFruit + " is found in position " + i;
+    // document.getElementById("demo").innerHTML = arrayOfFruit + " is found in position " + i;
+}
